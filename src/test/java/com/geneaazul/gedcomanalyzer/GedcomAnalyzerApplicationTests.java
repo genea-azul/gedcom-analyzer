77f3a10afc6c6a708803d52f3914c9703c5ceb05
genea-azul <genea.azul@gmail.com>
2022-12-23 15:29:25 -0300

    Spring Boot initial commit

---
+++ src/test/java/com/geneaazul/gedcomanalyzer/GedcomAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geneaazul.gedcomanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedcomAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
