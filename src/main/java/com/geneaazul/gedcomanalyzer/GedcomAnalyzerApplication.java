8c2da67c6606fa645acc3a2349175b5d388c0d09
Genea Azul <121312067+genea-azul@users.noreply.github.com>
2022-12-23 15:49:37 -0300

    Spring Boot initial commit (#1)

---
+++ src/main/java/com/geneaazul/gedcomanalyzer/GedcomAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.geneaazul.gedcomanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedcomAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GedcomAnalyzerApplication.class, args);
+    }
+
+}
