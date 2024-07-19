f50735e2e5729a7558e316ae717d5e85caf3e5c8
genea-azul <genea.azul@gmail.com>
2024-07-19 14:05:32 -0300

    Update dependency install scripts

--- scripts/dependency-install-icu4j.sh
+++ scripts/dependency-install-icu4j.sh
@@ -5,14 +5,10 @@
 #
 #   git clone --depth 1 -b maint/maint-75 https://github.com/unicode-org/icu
 
-userMvnRepo=~/.m2/repository
-projMvnRepo=./.mvn/repo
-
 artifactProjDir=../icu/icu4j
-artifactMvnRepoDir=/com/ibm/icu
-artifactVersion=75.1
 artifactGitMain=main
 artifactGitBranch=maint/maint-75
+artifactVersion=75.1
 
 # Update branch (enable if not cloning previously)
 # git -C ${artifactProjDir} pull
@@ -25,24 +21,5 @@ artifactGitBranch=maint/maint-75
 # git -C ${artifactProjDir} checkout ${artifactGitMain}
 
 # Copy dependencies to local Maven repo
-./mvnw install:install-file \
-      -Dfile=${artifactProjDir}/main/core/target/core-${artifactVersion}.jar \
-      -DgroupId=com.ibm.icu \
-      -DartifactId=icu4j-core \
-      -Dversion=${artifactVersion} \
-      -Dpackaging=jar \
-      -DgeneratePom=true \
-      -DcreateChecksum=true
-
-./mvnw install:install-file \
-      -Dfile=${artifactProjDir}/main/translit/target/translit-${artifactVersion}.jar \
-      -DgroupId=com.ibm.icu \
-      -DartifactId=icu4j-translit \
-      -Dversion=${artifactVersion} \
-      -Dpackaging=jar \
-      -DgeneratePom=true \
-      -DcreateChecksum=true
-
-mkdir -p ${projMvnRepo}${artifactMvnRepoDir}
-cp -r ${userMvnRepo}${artifactMvnRepoDir}/icu4j-core ${projMvnRepo}${artifactMvnRepoDir}
-cp -r ${userMvnRepo}${artifactMvnRepoDir}/icu4j-translit ${projMvnRepo}${artifactMvnRepoDir}
+./scripts/dependency-install-file.sh "../icu/icu4j" "/com/ibm/icu" "com.ibm.icu" "/main/core" "core" "icu4j-core" ${artifactVersion}
+./scripts/dependency-install-file.sh "../icu/icu4j" "/com/ibm/icu" "com.ibm.icu" "/main/translit" "translit" "icu4j-translit" ${artifactVersion}
